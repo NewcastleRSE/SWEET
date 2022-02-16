79d1d8cf609f88ff053b4bbe47b7bec292dc717f
Jess <j.kay2@newcastle.ac.uk>
2022-02-16 10:43:34 +0000

    Secrets_*.py

--- SWEET/static/scripts/renderers/contacts.js
+++ SWEET/static/scripts/renderers/contacts.js
@@ -3,7 +3,7 @@ export function contactListRenderer(section) {
 
     let holder = document.createElement("section");
     holder.classList.add("contact-list");
-    section.contacts.forEach(contact => this.render(contact.type == "contact"? contact: { type: "contact", "contact": contact}).then(node => holder.appendChild(node)));
+    section.contacts.forEach(contact => this.render(contact.type == "contact" ? contact : { type: "contact", "contact": contact }).then(node => holder.appendChild(node)));
 
     return holder;
 }
@@ -18,8 +18,8 @@ export function contactRenderer(section) {
 
     holder.innerHTML = `
         <div class="my-3">
-            <button class="edit" style="float: left;"></button>
-            <button class="delete" style="float: left; clear: left;"></button>
+            <button class="edit" style="float: left; "></button>
+            <button class="delete" style="float: right; clear: right;"></button>
             <h5>Key Contact: ${contact.type}</h5>
             <div class="contact-body">
                 <strong>Name</strong>: <span class="contact-name">${contact.name}</span><br>
@@ -34,12 +34,12 @@ export function contactRenderer(section) {
         modal.title.textContent = "Edit Contact";
         modal.footer.innerHTML = "<button type='button' class='close btn btn-secondary'>Cancel</button> <button type='submit' class='submit btn btn-primary'>Save Changes</button>"
 
-        this.render({ type: "contact-form", "contact": contact}).then(form => {
+        this.render({ type: "contact-form", "contact": contact }).then(form => {
             form.$opener = holder;
             form.$modal = modal;
 
             modal.body.appendChild(form);
-            
+
             modal.footer.querySelector("button.submit").setAttribute("form", form.getAttribute("id"));
             modal.footer.querySelector("button.close").addEventListener("click", e => modal.hide());
 
@@ -50,7 +50,7 @@ export function contactRenderer(section) {
     holder.querySelector("button.delete").addEventListener("click", e => {
         if (window.confirm(`Delete ${contact.name} from your contacts?\n(this change cannot be undone)`)) {
             this.post("/myapp/mycontacts/delete/", contact).then(response => response.json())
-            .then(result => holder.remove())
+                .then(result => holder.remove())
         }
     })
 
@@ -63,7 +63,7 @@ export function contactFormRenderer(section) {
     let edit = ("contact" in section);
 
     let form = document.createElement("form");
-    form.setAttribute("id", edit? section.contact.name.toLowerCase().replaceAll(" ", "_"): "new-contact")
+    form.setAttribute("id", edit ? section.contact.name.toLowerCase().replaceAll(" ", "_") : "new-contact")
 
     form.innerHTML = `
         <label for="contact-type" class="p-2">Who is this person?</label><select id="contact-person-type" name="type">
@@ -72,9 +72,9 @@ export function contactFormRenderer(section) {
         <option>GP Practice</option>
         <option>Pharmacist</option>
         <option>Other</option></select><br>
-        <label for="contact-name" class="p-2 lbl-small">Name:</label> <input type="text" id="contact-name" class="my-1" name="name"${edit?` value="${section.contact.name}"`:""} minlength="2" maxlength="25" required><br>
-        <label for="contact-phone" class="p-2 lbl-small">Phone:</label> <input type="tel" id="contact-phone" class="my-1" name="phone"${edit?` value="${section.contact.phone}"`:""} minlength="6" maxlength="15"><br>
-        <label for="contact-email" class="p-2 lbl-small">Email:</label> <input type="email" id="contact-email" class="my-1"name="email"${edit?` value="${section.contact.email}"`:""} minlength="6" maxlength="50"><br>
+        <label for="contact-name" class="p-2 lbl-small">Name:</label> <input type="text" id="contact-name" class="my-1" name="name"${edit ? ` value="${section.contact.name}"` : ""} minlength="2" maxlength="25" required><br>
+        <label for="contact-phone" class="p-2 lbl-small">Phone:</label> <input type="tel" id="contact-phone" class="my-1" name="phone"${edit ? ` value="${section.contact.phone}"` : ""} minlength="6" maxlength="15"><br>
+        <label for="contact-email" class="p-2 lbl-small">Email:</label> <input type="email" id="contact-email" class="my-1"name="email"${edit ? ` value="${section.contact.email}"` : ""} minlength="6" maxlength="50"><br>
     `
 
     if (edit) {
@@ -85,7 +85,7 @@ export function contactFormRenderer(section) {
     form.addEventListener("submit", e => {
         e.preventDefault();
 
-        let contact =  {
+        let contact = {
             type: form.elements["type"].value,
             name: form.elements["name"].value,
             phone: form.elements["phone"].value,
@@ -93,7 +93,7 @@ export function contactFormRenderer(section) {
         }
 
         let poster = edit ?
-            this.post("/myapp/mycontacts/update/", { newcontact: contact, oldcontact: section.contact}) : 
+            this.post("/myapp/mycontacts/update/", { newcontact: contact, oldcontact: section.contact }) :
             this.post("/myapp/mycontacts/add/", contact);
 
         poster.then(() => {
@@ -104,7 +104,7 @@ export function contactFormRenderer(section) {
             }
 
             if (form.$list) {
-                this.render({ type: "contact", contact: contact}).then(node => form.$list.appendChild(node))
+                this.render({ type: "contact", contact: contact }).then(node => form.$list.appendChild(node))
             }
 
             if (form.$modal) form.$modal.hide();
@@ -121,33 +121,33 @@ export function contactPageRenderer(section) {
     holder.classList.add("all-contacts")
 
     fetch("/myapp/mycontacts").then(response => response.json())
-    .then(output => {
-        this.render({ type: "contact-list", contacts: output.contacts.map(c => { return { type: "contact", contact: c}; })})
-        .then(list => {
-            holder.appendChild(list);
-            holder.insertAdjacentHTML("beforeend", `
+        .then(output => {
+            this.render({ type: "contact-list", contacts: output.contacts.map(c => { return { type: "contact", contact: c }; }) })
+                .then(list => {
+                    holder.appendChild(list);
+                    holder.insertAdjacentHTML("beforeend", `
                 <div class="contact-page-new"><button type="button" class="btn btn-primary add">Add new contact</button></div>
             `)
 
-            holder.querySelector("button.add").addEventListener("click", e => {
-                let modal = this.createModal(true);
-                modal.title.textContent = "Add Contact";
-                modal.footer.innerHTML = "<button type='button' class='close btn btn-secondary'>Cancel</button> <button type='submit' class='submit btn btn-primary'>Save Changes</button>"
-        
-                this.render({ type: "contact-form"}).then(form => {
-                    form.$modal = modal;
-                    form.$list = list;
-        
-                    modal.body.appendChild(form);
-                    
-                    modal.footer.querySelector("button.close").addEventListener("click", e => modal.hide());
-                    modal.footer.querySelector("[type='submit']").setAttribute("form", form.getAttribute("id"))
-        
-                    modal.show();
+                    holder.querySelector("button.add").addEventListener("click", e => {
+                        let modal = this.createModal(true);
+                        modal.title.textContent = "Add Contact";
+                        modal.footer.innerHTML = "<button type='button' class='close btn btn-secondary'>Cancel</button> <button type='submit' class='submit btn btn-primary'>Save Changes</button>"
+
+                        this.render({ type: "contact-form" }).then(form => {
+                            form.$modal = modal;
+                            form.$list = list;
+
+                            modal.body.appendChild(form);
+
+                            modal.footer.querySelector("button.close").addEventListener("click", e => modal.hide());
+                            modal.footer.querySelector("[type='submit']").setAttribute("form", form.getAttribute("id"))
+
+                            modal.show();
+                        })
+                    })
                 })
-            })
         })
-    })
 
     return holder;
 }
