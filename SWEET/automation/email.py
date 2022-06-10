07b3758d792ea1035f656ac90d7c74d26b60931c
KateCourt <49239607+KateCourt@users.noreply.github.com>
2022-06-10 14:34:25 +0100

    being issue #285 : add function that checks for presence of last name before creating email messages

--- SWEET/automation/email.py
+++ SWEET/automation/email.py
@@ -35,6 +35,12 @@ def _send_message(to, template="welcome", **kwargs):
 
     _send(msg)
 
+def create_name(user):
+    if user['lastName'] and len(user['lastName']) > 0:
+        return f"{user['firstName']} {user['lastName']}"
+    else:
+        return f"{user['firstName']}"
+
 def send_welcome(user):
     fullname = f"{user['firstName']} {user['lastName']}"
     _send_message(f"{fullname} <{user['email']}>", "welcome", fullname=fullname)
@@ -63,5 +69,5 @@ def send_profiler_due(user):
     _send_message(f"{fullname} <{user['email']}>", "profiler_reminder", fullname=fullname, hostname=hostname)
 
 def send_goal_reminder(detail):
-    fullname = f"{detail['firstName']} {detail['lastName']}"
+    fullname = create_name
     _send_message(f"{fullname} <{detail['email']}>", "goal_reminder", fullname=fullname, hostname=hostname, shorttype=detail['shortType'], longtype=detail['longType'])
