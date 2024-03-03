787348923ab8325e90e7fc82eb4854788eb475a7
Mark Turner <mark.turner@ncl.ac.uk>
2024-03-03 13:22:53 +0000

    send nudges at 18:00

--- SWEET/data/userdata.py
+++ SWEET/data/userdata.py
@@ -811,7 +811,7 @@ def get_schedule(day):
                     if(user['email'] == 'mark.turner@ncl.ac.uk'):
                         capture_message('month: ' + str(i))
                     if (init_date + relativedelta(months=i) == today):
-                        sched = {'userID':user['userID'], 'firstName': user['firstName'], 'lastName': user['lastName'], 'to': user['email'],'method': 'email', 'type': 'nudge-' + str(i) +'_month'}
+                        sched = {'userID':user['userID'], 'firstName': user['firstName'], 'lastName': user['lastName'], 'to': user['email'],'method': 'email', 'type': 'nudge-' + str(i) +'_month', 'time': '18:00'}
                         schedule.append(sched)
                         if(user['email'] == 'mark.turner@ncl.ac.uk'):
                             capture_message('added scheduled nudge-{i}_month email')
